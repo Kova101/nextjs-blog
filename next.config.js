47e6ea10d306884c582db8ec976317701548b33d
Roland Kovacsics <roland.kovacsics@acquia.com>
2023-03-16 12:07:14 +0100

    Change the project to fully Static

--- next.config.js
+++ next.config.js
@@ -2,53 +2,8 @@
  * @type {import('next').NextConfig}
  **/
 const nextConfig = {
-    async headers() {
-        return [
-            // To test if Fastly cache is cleared and the new data is instantly displayed.
-            {
-                source: '/',
-                headers: [
-                    {
-                        key: 'Surrogate-Control',
-                        value: 'max-age=31557600',
-                    },
-                    {
-                        key: 'Cache-Control',
-                        value: 'no-store, max-age=0',
-                    },
-                ],
-            },
-            // 1 hour cache in fastly and 5 minutes cache in browser.
-            // To test that after Fastly clear the content is displayed after 5 minutes.
-            {
-                source: '/about',
-                headers: [
-                    {
-                        key: 'Surrogate-Control',
-                        value: 'max-age=3600',
-                    },
-                    {
-                        key: 'Cache-Control',
-                        value: 'max-age=300',
-                    },
-                ],
-            },
-            // No cache in Fastly but 5 minutes cache in browser.
-            {
-                source: '/article/:id',
-                headers: [
-                    {
-                        key: 'Surrogate-Control',
-                        value: 'no-store, max-age=0',
-                    },
-                    {
-                        key: 'Cache-Control',
-                        value: 'max-age=300',
-                    },
-                ],
-            },
-        ]
-    },
+    output: 'export',
+    trailingSlash: true,
 }
 
 module.exports = nextConfig
